e67591c6791f3cb2dfb54348c484d32a4ac205b2
Junhee Woo <dyoona@gmail.com>
2015-06-05 14:01:58 +0900

    간단히 로깅을 해보자.

--- main.go
+++ main.go
@@ -13,6 +13,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprintf(w, "{ \"code\": %d, \"message\": \"method not allowed\" }", http.StatusMethodNotAllowed)
 	} else {
+		fmt.Println("Header :", r.Header)
+		fmt.Println("URL :", r.URL)
+		fmt.Println("Path :", r.URL.Path)
+		fmt.Println("RawQuery :", r.URL.RawQuery)
+
 		fmt.Fprintf(w, "{ \"path\": \"%s\" }", r.URL.Path[1:])	
 	}
     
@@ -20,5 +25,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/", handler)
+    log.Printf("API Server Ready")
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
